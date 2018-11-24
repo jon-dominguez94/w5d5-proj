15cad16f06cf2bd9fea9d3475383f10152a5adf9
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-24 07:41:34 -0800

    Declare printStacks and promptMove prototype methods

--- css-friends-06/skeleton/hanoi.js
+++ css-friends-06/skeleton/hanoi.js
@@ -1,5 +1,20 @@
 class Hanoi {
-  run() {
-    
+
+  constructor() {
+    this.towers = [ [3,2,1], [], [] ];
   }
+
 }
+
+Hanoi.prototype.printStacks() {
+
+}
+
+Hanoi.prototype.promptMove() {
+  
+}
+
+Hanoi.prototype.run = function () {
+  Hanoi.printStacks();
+  Hanoi.promptMove();
+};
