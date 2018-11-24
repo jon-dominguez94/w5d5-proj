800b7bafc78d381f5ca9592ad0a124a34ddeacfe
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-24 02:48:01 -0800

    Implement tick

--- css-friends-06/skeleton/callbacks.js
+++ css-friends-06/skeleton/callbacks.js
@@ -1,17 +1,18 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.  
+
     const currTime = new Date();
 
-    // 2. Store the hours, minutes, and seconds.
     this.hours = currTime.getHours();
     this.minutes = currTime.getMinutes();
     this.seconds = currTime.getSeconds();
 
-    // 3. Call printTime.
     this.printTime();
 
-    // 4. Schedule the tick at 1 second intervals.  
     setInterval(this._tick.bind(this), 1000);
   }
 
@@ -25,6 +26,15 @@ class Clock {
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes += 1;
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.hours = (this.hours + 1) % 24;
+      }
+    }
   }
 }
 
