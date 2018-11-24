221ee0ecd96800ca9832ef34f9b5d5d36464f14f
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-11-24 03:10:20 -0800

    Fix clock exercise file name and printing bugs

--- css-friends-06/skeleton/clock.js
+++ css-friends-06/skeleton/clock.js
@@ -1,28 +1,41 @@
 class Clock {
   constructor() {
     // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.  
+
     const currTime = new Date();
 
-    // 2. Store the hours, minutes, and seconds.
     this.hours = currTime.getHours();
     this.minutes = currTime.getMinutes();
     this.seconds = currTime.getSeconds();
 
-    // 3. Call printTime.
     this.printTime();
 
-    // 4. Schedule the tick at 1 second intervals.  
     setInterval(this._tick.bind(this), 1000);
   }
 
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
+    const output = [this.hours, this.minutes, this.seconds].join(":");
+    console.log(output);
   }
 
   _tick() {
     // 1. Increment the time by one second.
     // 2. Call printTime.
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes += 1;
+      if (this.minutes === 60) {
+        this.minutes = 0;
+        this.hours = (this.hours + 1) % 24;
+      }
+    }
+    this.printTime();
   }
 }
 
